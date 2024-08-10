ab9bacacdc56462608026c557e141f3aa1361c33
mayank jain <109034440+MjMaster321@users.noreply.github.com>
2024-08-10 22:41:47 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+console.log("2 > 1"); // result will be true
+console.log("02 > 1"); // result will be true 
+
+// as java automatically converts those strings into number for comparison case
+
+console.log(null > 0);  //false
+console.log(null == 0);  // false 
+console.log(null >= 0);   // true
+
+// ===   it strict checks and dont approve comparison if the data types are different 
+
+console.log("2" === 2); // false 
