6807383a0e3ce3ace1031a5c6f43aa3e68c20de1
mayank jain <109034440+MjMaster321@users.noreply.github.com>
2024-08-10 13:03:45 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS codes as newer version 
+
+// alert (3+3) // we are using node.js, not browser 
+
+// code readability should be high ;
+// js automatically avoids extra spaces; but readability is important 
+let name = "mayank" 
+let age = 18 
+let isLoggedIn = true 
+
+
+// number = 2 to the power 53
+// bigint =
+// string = ""
+//boolean = true/false 
+// null = standalone value 
+// undefined = value is yet not defined in the function 
+// symbol = unique 
+
+
+// object 
+
+console.log(typeof "Mayank")  // typeof is used to know the type of function;string in this case 
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null);     // object
