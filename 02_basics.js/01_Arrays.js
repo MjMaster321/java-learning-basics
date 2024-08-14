3f29a84ca7cce33044eb35e8437e51d1ede21c02
mayank jain <109034440+MjMaster321@users.noreply.github.com>
2024-08-14 21:11:32 +0530

    Delete 02_basics.js directory

--- 02_basics.js/01_Arrays.js
+++
@@ -1,6 +0,0 @@
-const myArr = [0, 1, 2, 3, 4, 5 ] // [] = brackets; () = parenthesis ; {} = braces ; 
-
-const myHeroes = ["spiderman", "Batman", "IronMan"]; 
-const myArr1 = new Array(1,2,3,4);
-
-console.log(myArr[0]); //
