84933baa77287f70065e62a65ef2647d85efe524
mayank jain <109034440+MjMaster321@users.noreply.github.com>
2024-08-21 16:01:42 +0000

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,22 @@
+const temperature = 41
+
+if(temperature < 41){
+    console.log("less than 50"); 
+}
+  else {
+    console.log("temperature is greater than 50"); 
+  }                                                           // temperature is greater than 50 
+  console.log("exectued")                                      // above line along with executed 
+
+  const balance = 1000
+   
+if (balance < 500){
+    console.log("Balance less than 500")
+} else if (balance < 750){
+    console.log("Balance is less than 750")
+} else if (balance < 900){
+    console.log("Balance is less than 900")
+} else {
+    console.log("Balance is less than 1500")
+}                                                             // result = Balance is less than 1500
+
