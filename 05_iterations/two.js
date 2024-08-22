c4c54f81e62c1e574ae64b2e31b2a68374f75664
mayank jain <109034440+MjMaster321@users.noreply.github.com>
2024-08-22 15:00:28 +0000

    two

---
+++ 05_iterations/two.js
@@ -0,0 +1,42 @@
+// while and do while loop 
+
+let index = 0
+while (index <=10) {
+    console.log(index); 
+    index = index + 2
+}                                      // 0 2 4 6 8 10
+
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <= 10);                 // score is 1
+                                       // score is 2
+                                       // score is 3
+                                      // score is 4
+                                      // score is 5
+                                      // score is 6
+                                      // score is 7
+                                     // score is 8
+                                       // score is 9
+                                      // score is 10
+                
+let score1 = 11
+
+do {
+    console.log(`score is ${score1}`)
+    score1++
+} while (score1 <= 10);                      // score is 11
+
+
+                         
+
+
+
+
+
+
+
+
