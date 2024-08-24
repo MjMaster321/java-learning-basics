3f6a2f82b2e1caa0d425af08f72dfca1374f6ca4
mayank jain <109034440+MjMaster321@users.noreply.github.com>
2024-08-24 12:08:09 +0000

    four

---
+++ 05_iterations/four.js
@@ -0,0 +1,26 @@
+const coding = ["ram", "shyam", "Mohan", "Sohan"]
+
+coding.forEach( function (item) {
+    console.log(item); 
+} )                  
+
+const myCoding = [
+    {
+      languageName: "Javascript", 
+      LanguageFileName: "Js",
+}, 
+    {
+      languageName: "Python", 
+      LanguageFileName: "Py",
+}, 
+    {
+      languageName: "Java", 
+      LanguageFileName: "Java",
+}, 
+
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+})                                      //  javascript Java Python
+
